779b8741eb5804d30995b9570b2cd224d47cee56
Anatoly Danilov <danilov.aa@mail.ru>
2018-09-05 08:42:51 +0300

    very duplicaty

---
+++ src/main/java/com/example/demoofmanyduplicates/WeirdDuplicateComponent.java
@@ -0,0 +1,520 @@
+package com.example.demoofmanyduplicates;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class WeirdDuplicateComponent {
+
+    public void doSomethingWeird() {
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+    }
+
+    public void doSomethingWeirdAgain() {
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        log.info("Doing duplicated stuff");
+        for (int i = 0; i < 100213; i++) {
+            log.info("no guessing");
+            log.error("no guessing");
+            log.info("no guessing");
+        }
+    }
+
+}
